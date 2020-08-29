02dc09e3dc1496f6d25a08dfc7caa0c66114dc49
Soham Naha <sohamnaha.sn@gmail.com>
2020-08-29 15:30:17 +0530

    Additions before afternoon of 29th Aug

--- assignment-1-transformations/1/code/myShrinkImageByFactorD.py
+++ assignment-1-transformations/1/code/myShrinkImageByFactorD.py
@@ -18,18 +18,17 @@ def myShrinkImageByFactorD(d,cmap="gray"):
     input_file = "../data/circles_concentric.png"
     input_image = mpimg.imread(input_file,format="png")
     num_plots = len(d)+1
+    output_images = []
     
-    width = input_image.shape[0]
-    height = input_image.shape[1]
+    width,height = input_image.shape
+    #height = input_image.shape[1]
     
-    fig,axes = plt.subplots(1,num_plots, constrained_layout=True, gridspec_kw={'width_ratios':[4,2,1]})
+    #fig,axes = plt.subplots(1,num_plots, constrained_layout=True, gridspec_kw={'width_ratios':[1,1,1]})
     
-    #vmin = 0
-    #vmax = 255
 
-    axes[0].imshow(input_image,cmap=cmap)
-    axes[0].axis("on")
-    
+    #axes[0].imshow(input_image,cmap=cmap)
+    #axes[0].axis("on")
+    #axes[0].set_title("Original Image")
     count = 0
 
     for i in d:
@@ -39,18 +38,32 @@ def myShrinkImageByFactorD(d,cmap="gray"):
         output = zeros((new_width,new_height))
         for W in range(new_width):
             for H in range(new_height):
-                #if W%i==0 and H%i==0:
-                    #new_width = int(W*i)
-                    #new_height = int(H*i)
                 output[W][H] = input_image[W*i][H*i]
+        
+        output_images.append(output)
 
-        im = axes[count].imshow(output, cmap=cmap)
-        axes[count].axis("on")
+        #im = axes[count].imshow(output, cmap=cmap)
+        #axes[count].axis("on")
+        #axes[count].set_title("Shrinked by size d="+str(i))
 
-    #plt.subplots_adjust(right=0.8)
-    #cax,kw = mpl.colorbar.make_axes([ax for ax in axes.flat])
-    cbar = fig.colorbar(im,ax=axes.ravel().tolist(),ticks=[0,1],shrink=0.45)
-    cbar.ax.set_yticklabels([0,255])
-    plt.show()
+    #cbar = fig.colorbar(im,ax=axes.ravel().tolist(),shrink=0.45)
+    #plt.show()
+    
+    parameters = {'axes.titlesize': 10}
+    plt.rcParams.update(parameters)
+
+    fig,axes = plt.subplots(1,num_plots, constrained_layout=True)
+    
+
+    axes[0].imshow(input_image,cmap=cmap)
+    axes[0].axis("on")
+    axes[0].set_title("Original Image")
+    for i in range(count):
+        im = axes[i+1].imshow(output_images[i], cmap=cmap)
+        axes[i+1].axis("on")
+        axes[i+1].set_title("Shrinked by size d="+str(d[i]))
+
+    cbar = fig.colorbar(im,ax=axes.ravel().tolist(),shrink=0.35)
+    plt.savefig("../data/Shrinkage.png",bbox_inches="tight",pad=-1)
 
 myShrinkImageByFactorD([2,3])
