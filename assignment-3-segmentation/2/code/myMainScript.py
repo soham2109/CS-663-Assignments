ab379f4d597028cc1b4e3ca9a1b2aa01dd800ad4
Soham <snaha@sandesh.ee.iitb.ac.in>
2020-10-17 22:48:48 +0530

    iterations added

--- assignment-3-segmentation/2/code/myMainScript.py
+++ assignment-3-segmentation/2/code/myMainScript.py
@@ -1,7 +1,9 @@
 filenames = ["baboonColor.png","bird.jpg","flower.jpg"]
 foldername = "../data/"
+iterations = [30,40,40]
+
 
 from myMeanShift import meanShift
 
-for image in filenames:
-    meanShift(foldername+image)
+for i in range(len(filenames)):
+    meanShift(foldername+filenames[i],intensity_sigma=3.0,num_iter=iterations[i])
