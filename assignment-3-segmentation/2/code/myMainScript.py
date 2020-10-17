aaa557df98de0a6e1c62733832165dd7ccd03790
Soham <snaha@sandesh.ee.iitb.ac.in>
2020-10-17 23:41:19 +0530

    Image uploads

--- assignment-3-segmentation/2/code/myMainScript.py
+++ assignment-3-segmentation/2/code/myMainScript.py
@@ -6,4 +6,4 @@ iterations = [30,40,40]
 from myMeanShift import meanShift
 
 for i in range(len(filenames)):
-    meanShift(foldername+filenames[i],intensity_sigma=3.0,num_iter=iterations[i])
+    meanShift(foldername+filenames[i],intensity_sigma=0.1,num_iter=iterations[i])
