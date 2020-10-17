90370d57c642ea78046ff8f39cdb2e47d8f9e284
Soham Naha <sohamnaha.sn@gmail.com>
2020-10-17 19:18:15 +0530

    MeanShift python file

---
+++ assignment-3-segmentation/2/code/myMeanShift.py
@@ -0,0 +1,122 @@
+import cv2
+import numpy as np
+import sys,os
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+def plot_and_save(output_image,input_image,filename):
+    name = filename.rstrip("\n").split("/")[-1].split(".")[0]
+    fig,axes = plt.subplots(1,2, constrained_layout=True)
+    axes[0].imshow(input_image,cmap=cmap)
+    axes[0].axis("on")
+    axes[0].set_title("Original Image")
+    im = axes[1].imshow(output_image)
+    axes[1].axis("on")
+    axes[1].set_title("MeanShift Image")
+    cbar = fig.colorbar(im,ax=axes.ravel().tolist(),shrink=0.45)
+    
+    plt.savefig("../images/"+name+"MeanShiftcombined_.png",bbox_inches="tight",pad=-1)
+
+    plt.imsave("../images/" + name+"MeanShift.png",output_image)
+
+def im2double(im,d):    
+    im1 = im[:,:,0]
+    im2 = im[:,:,1]
+    im3 = im[:,:,2]
+    out1 = (im1.astype(np.float64) - np.min(im1.ravel())) / (np.max(im1.ravel()) - np.min(im1.ravel()))
+    out2 = (im2.astype(np.float64) - np.min(im2.ravel())) / (np.max(im2.ravel()) - np.min(im2.ravel()))
+    out3 = (im3.astype(np.float64) - np.min(im3.ravel())) / (np.max(im3.ravel()) - np.min(im3.ravel()))
+    out = cv2.merge((out1,out2,out3))
+    return out
+
+def meanShift(filename,intensity_sigma=0.1,spatial_sigma=11.0,num_iter=30):
+    
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    
+    r,c,d = img.shape
+    img = im2double(img,d)
+    gaussian_blur = cv2.GaussianBlur(img, (5,5), 1.0)
+
+    row,col = 128,128
+    newimg = cv2.resize(img,(row,col))
+
+    
+    result1 = np.zeros((row,col))
+    result2 = np.zeros((row,col))
+    result3 = np.zeros((row,col))
+    
+    #intensity Gaussian standard deviation
+    h=intensity_sigma
+    
+    #spatial Gaussian parameters
+    sigma = spatial_sigma
+    window_size = 7
+    padded = np.concatenate((np.concatenate((np.zeros((row,window_size,3)), newimg),axis=1), np.zeros((row,window_size,3))),axis=1)
+
+    padded = np.concatenate((np.concatenate((np.zeros((window_size,col+2*(window_size),3)),padded),axis=0), np.zeros((window_size, col+2*(window_size),3))),axis=0)
+    #padded = np.zeros((row+2*window_size,col+2*window_size))
+    r,c,d = padded.shape
+    #r,c = padded.shape
+    print(padded.shape)
+
+    #isotropic
+    #Spatial
+    spatial = np.zeros((2*window_size+1,2*window_size+1))
+    for is1 in range(2*window_size+1):
+        for is2 in range(2*window_size+1):
+            spatial[is1][is2] = ((is1-window_size)**2+(is2-window_size)**2)**0.5
+    spatial = np.exp(-(spatial/sigma)**2)
+
+    
+    for idx1 in tqdm(range(window_size,r-window_size)):
+        for idx2 in range(window_size,c-window_size):
+            window = padded[idx1-window_size:idx1+window_size+1, idx2-window_size:idx2+window_size+1]
+            #print(newimg[idx1][idx2])
+            (x1,x2,x3) = newimg[idx1-window_size][idx2-window_size]
+            N1 = 0.0 #numerator
+            D1 = 1.0 #denominator
+            N2 = 0.0 #numerator
+            D2 = 1.0 #denominator
+            N3 = 0.0 #numerator
+            D3 = 1.0 #denominator
+
+
+            for itern in range(num_iter): # number of iterations
+                for idx3 in range(2*window_size+1):
+                    for idx4 in range(2*window_size+1):
+                        (x_i1,x_i2,x_i3) = window[idx3][idx4]
+                        diff1 = abs(x1-x_i1)
+                        diff2 = abs(x2-x_i2)
+                        diff3 = abs(x3-x_i3)
+
+                        d1 = np.exp(-(diff1/h)**2)*spatial[idx3][idx4]
+                        n1 = x_i1*d1
+                        N1 += n1
+                        D1 += d1
+
+                        d2 = np.exp(-(diff2/h)**2)*spatial[idx3][idx4]
+                        n2 = x_i2*d2
+                        N2 += n2
+                        D2 += d2
+
+                        d3 = np.exp(-(diff3/h)**2)*spatial[idx3][idx4]
+                        n3 = x_i3*d3
+                        N3 += n3
+                        D3 += d3
+
+                x1 = float(N1)/D1 # in each iteration, x changes
+                x2 = float(N2)/D2 # in each iteration, x changes
+                x3 = float(N3)/D3 # in each iteration, x changes
+
+            result1[idx1-window_size][idx2-window_size] = x1
+            result2[idx1-window_size][idx2-window_size] = x2
+            result3[idx1-window_size][idx2-window_size] = x3
+
+    result = cv2.merge((result1,result2,result3))
+    #print(result)
+    plot_and_save(result,newimg,filename)
+    
+if __name__=="__main__":
+    filename = sys.argv[1]
+    meanShift = (filename)
