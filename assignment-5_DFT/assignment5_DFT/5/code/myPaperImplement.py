91a2ff58764519f206c7a6b71749bc30008f5792
Soham Naha <sohamnaha.sn@gmail.com>
2020-11-15 13:48:13 +0530

    Q5 modified

--- assignment-5_DFT/assignment5_DFT/5/code/myPaperImplement.py
+++ assignment-5_DFT/assignment5_DFT/5/code/myPaperImplement.py
@@ -45,28 +45,44 @@ def spatial_translation(image, translation, verbose=True):
     return translated
 
 def calculate_Fourier(image):
-    return np.fft.fftshift(np.fft.fft2(image))
+    # return np.fft.fftshift(np.fft.fft2(image))
+    return np.fft.fft2(image)
 
 def cross_power_spectrum(orig, translated, verbose=True):
     
     orig_fourier = calculate_Fourier(orig)
     trans_fourier = calculate_Fourier(translated)
-    cross_power = orig_fourier * np.conj(trans_fourier)
-    cross_power = cross_power/np.abs(orig_fourier*trans_fourier)
-    inv_cross = np.fft.ifftshift(np.fft.ifft2(cross_power))
-    mag_inv_cross = np.abs(inv_cross)
-    print(mag_inv_cross[mag_inv_cross>0])
+    cross_power = np.fft.ifft2(np.conj(orig_fourier)*(trans_fourier))
+    eps = 1e-15
+    # print(cross_power)
+    # print(np.matmul(orig_fourier,trans_fourier))
+    cross_power = np.abs(cross_power/(np.matmul(np.abs(orig_fourier),np.abs(trans_fourier)) + eps))
+    ir = np.abs(np.fft.ifft2((orig_fourier * trans_fourier.conjugate()) / (np.abs(orig_fourier) * np.abs(trans_fourier)+eps)))
+    # inv_cross = np.fft.ifftshift(np.fft.ifft2(cross_power))
+    # inv_cross = np.fft.ifft2(cross_power)
+    # mag_inv_cross = np.abs(inv_cross)
+    # print(mag_inv_cross)
     
     if verbose:
         plt.figure()
-        plt.imshow(mag_inv_cross)
-        plt.show()
+        plt.imshow(cross_power, cmap="jet")
+        plt.colorbar()
+        plt.savefig("../images/CrossPowerSpectrum.png",bbox_inches="tight",cmap="jet")
+    
+    r,c = orig.shape
+    t0, t1 = np.unravel_index(np.argmax(ir), orig.shape)
+    if t0 >r//2:
+        t0 -= r
+    if t1 >c//2:
+        t1 -= c
+    return [t1, t0]
 
 if __name__=="__main__":
     size = 300
     start = (50,50)
     shape = (50,70)
     orig_rect = create_rectangle(size, start, shape)
-    orig_trans = spatial_translation(orig_rect, (-30, 70))
-    cross_power_spectrum(orig_rect, orig_trans)
+    orig_trans = spatial_translation(orig_rect, (30, 70))
+    t0,t1 = cross_power_spectrum(orig_rect.astype(np.float), orig_trans.astype(np.float))
+    print("tx: {}, ty: {}".format(t0, t1))
 
